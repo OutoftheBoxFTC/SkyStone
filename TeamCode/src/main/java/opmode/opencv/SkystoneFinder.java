2d818ed6638c73aceb91209d335dd9f7d486f9bf
chiraagChakravarthy <beaverduck99@gmail.com>
2019-10-25 19:55:38 -0400

    Finished Skystone Finder

--- TeamCode/src/main/java/opmode/opencv/SkystoneFinder.java
+++ TeamCode/src/main/java/opmode/opencv/SkystoneFinder.java
@@ -17,52 +17,43 @@ import org.openftc.easyopencv.OpenCvWebcam;
 
 @TeleOp(name = "Skystone Finder")
 public class SkystoneFinder extends LinearOpMode {
+    //0-1 is in position 0, and 2-3 is in position 1
     OpenCvCamera webcam;
-    int x1, y1, x2, y2;
     private Point[] points;
+    private int pos;
+    private double avg1G, avg2G;
     @Override
     public void runOpMode() {
+        pos = 0;
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = new OpenCvWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         webcam.openCameraDevice();
-        points = new Point[]{};
+        points = new Point[]{new Point(358, 359), new Point(429, 383), new Point(358, 209), new Point(429, 235)};
         webcam.setPipeline(new OpenCvPipeline() {
-
             @Override
             public Mat processFrame(Mat input) {
                 double[] color1 = avgColor(input, points[0], points[1]), color2 = avgColor(input, points[2], points[3]);
                 double avg1 = (color1[0]+color1[1]+color1[2])/3, avg2 = (color2[0]+color1[1]+color1[2])/3;
+                if(Math.abs(avg1-avg2)<20){
+                    pos = 2;
+                } else if(avg1<avg2){
+                    pos = 0;
+                } else {
+                    pos = 1;
+                }
+                avg1G = avg1;
+                avg2G = avg2;
+                Imgproc.rectangle(input, points[0], points[1], new Scalar(255, 0, 0));//1: red
+                Imgproc.rectangle(input, points[2], points[3], new Scalar(0, 0, 255));//2: blue
                 return input;
             }
         });
         webcam.startStreaming(640, 480, OpenCvCameraRotation.SIDEWAYS_RIGHT);
         waitForStart();
         while (opModeIsActive()) {
-            if(gamepad1.dpad_up){
-                y1--;
-            } else if(gamepad1.dpad_down){
-                y1++;
-            }
-            if(gamepad1.dpad_left){
-                x1--;
-            } else if(gamepad1.dpad_right){
-                x1++;
-            }
-
-            if(gamepad2.dpad_up){
-                y2--;
-            } else if(gamepad2.dpad_down){
-                y2++;
-            }
-            if(gamepad2.dpad_left){
-                x2--;
-            } else if(gamepad2.dpad_right){
-                x2++;
-            }
-            telemetry.addData("x1", x1);
-            telemetry.addData("x2", x2);
-            telemetry.addData("y1", y1);
-            telemetry.addData("y2", y2);
+            telemetry.addData("pos", pos);
+            telemetry.addData("avg1", avg1G);
+            telemetry.addData("avg2", avg2G);
             telemetry.update();
             sleep(10*(gamepad1.a?2:1));
         }
